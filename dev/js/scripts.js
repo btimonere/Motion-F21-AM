5a421ee2cf6cecb62b7e3cfee3322d0b630f529e
btimonere <81781558+btimonere@users.noreply.github.com>
2021-10-02 20:36:38 -0400

    animating fade hero

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,4 +6,6 @@ const mainTL = gsap.timeline();
 //how long, what you want to do
 // mainTL.to("hero",{duration: 2, alpha:0});
 
-mainTL.from("#hero",{duration:3, alpha:0});
+mainTL.from("#hero",{duration:1.5, alpha:0})
+.from("#hero h1",{duration:1, alpha:0, x:"-200"})
+.from("#hero h2",{duration:1, alpha:0, x:"-200"});
