d9531e499908bdaa4761c1d9f682bf39cece1c66
btimonere <81781558+btimonere@users.noreply.github.com>
2021-10-02 20:53:20 -0400

    hero animation

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,10 +2,12 @@ import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 
+// const heroheight = document.querySelector("#hero")
+
 // name of TL, end or beginning, what to animate, 
 //how long, what you want to do
 // mainTL.to("hero",{duration: 2, alpha:0});
 
 mainTL.from("#hero",{duration:1.5, alpha:0})
 .from("#hero h1",{duration:1, alpha:0, x:"-200"})
-.from("#hero h2",{duration:1, alpha:0, x:"-200"});
+.from("#hero h2",{duration:1.5, alpha:0, x:"-200"});
