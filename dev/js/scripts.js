a9b78a81ee2b72f46478209d6cc4d2e907875f43
btimonere <81781558+btimonere@users.noreply.github.com>
2021-10-02 20:26:39 -0400

    fade animation

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,9 @@
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
+
+const mainTL = gsap.timeline();
+
+// name of TL, end or beginning, what to animate, 
+//how long, what you want to do
+// mainTL.to("hero",{duration: 2, alpha:0});
+
+mainTL.from("#hero",{duration:3, alpha:0});
