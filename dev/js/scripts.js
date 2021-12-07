8727458f8969e282e2562e19dd7f83ae88726064
btimonere <81781558+btimonere@users.noreply.github.com>
2021-12-07 16:25:25 -0500

    moving

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -14,14 +14,35 @@ const mainTL = gsap.timeline();
 
 function donutspin (){
     const tl = gsap.timeline();
-    //tl.to("#plaindoughnut",{rotation: "+=360", repeat: -1, ease: "Linear.easeNone", transformOrigin:"50% 50%"});
-    tl.from("#plaindoughnut", {duration: 1, ease: "none", y:"-100" });
+    tl.to("#plaindoughnut",{duration: 2, rotation: "-=330", ease: "none", transformOrigin:"50% 50%"});
+    //tl.from("#plaindoughnut", {duration: 1, ease: "none", y:"-100" });
     return tl;
 }
 
 function donutleft (){
     const tl = gsap.timeline();
-    tl.to("#plaindoughnut",{duration: 2, ease: "none", x: "-1000"});
+    tl.to("#plaindoughnut",{duration: 2, ease: "none", x: "-960"});
+    //tl.from("#plaindoughnut", {duration: 1, ease: "none", y:"-100" });
+    return tl;
+}
+
+function pinkright (){
+    const tl = gsap.timeline();
+    tl.to("#frostingcircle",{duration: 2, ease: "none", x: "500"});
+    //tl.from("#plaindoughnut", {duration: 1, ease: "none", y:"-100" });
+    return tl;
+}
+
+function sprinkspin (){
+    const tl = gsap.timeline();
+    tl.to("#sprinkdoughnut",{duration: 2, rotation: "+=330", ease: "none", transformOrigin:"50% 50%"});
+    //tl.from("#plaindoughnut", {duration: 1, ease: "none", y:"-100" });
+    return tl;
+}
+
+function sprinkright (){
+    const tl = gsap.timeline();
+    tl.to("#sprinkdoughnut",{duration: 2, ease: "none", x: "500"});
     //tl.from("#plaindoughnut", {duration: 1, ease: "none", y:"-100" });
     return tl;
 }
@@ -33,8 +54,12 @@ function donutleft (){
 
 
 
+
 mainTL.add(donutspin(),"same")
-.add(donutleft(),"same");
+.add(donutleft(),"same")
+.add(pinkright())
+.add(sprinkspin(),"spin")
+.add(sprinkright(),"spin");
 
 
 
